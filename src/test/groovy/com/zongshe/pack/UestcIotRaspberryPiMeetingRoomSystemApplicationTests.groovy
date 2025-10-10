dd126e6d9784de884dfac8a0df9c8b975fcba271
thenobody996 <411021455@qq.com>
2025-10-10 22:44:30 +0800

    项目初始化

---
+++ src/test/groovy/com/zongshe/pack/UestcIotRaspberryPiMeetingRoomSystemApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.zongshe.pack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UestcIotRaspberryPiMeetingRoomSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -17,4 +17,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8088',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+
+  }
 })
